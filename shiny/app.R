42caff7263875b12752f67e3a677ad5e7ef94941
Michelle Weirathmueller <1017259+michellejw@users.noreply.github.com>
2024-04-17 15:52:39 -0400

    remove the rsconnect folder from tracking; shiny updates

--- shiny/app.R
+++ shiny/app.R
@@ -3,26 +3,46 @@ library(bslib)
 library(plotly)
 library(dplyr)
 library(lubridate)
+library(corrplot)
 
 ui <- page_sidebar(
   title = "BioSound MBON Project Dashboard",
+  fillable=FALSE,
   sidebar = sidebar(
     title="More info",
     "App sidebar - nothing here yet but maybe we could add selections for things like location or sample rate etc."
     ),
   card(
+    height=400,
     card_header("Acoustic indices & fish annotations"),
-    plotlyOutput("plot") 
+    plotlyOutput("plot")
+  ),
+  card(
+    height=500,
+    card_header("Correlation matrix"),
+    plotOutput("corrPlot")
   )
 )
 
 server <- function(input, output) {
-  output$plot <- renderPlotly({
-    # Load the initial sample data (this will eventually be updated based on user selection)
-    indices_table <- read.csv("./data/keywest-withfish.csv")
-    indices_table <- indices_table %>%
-      mutate(datetime = ymd_hms(datetime))
+  
+  indices_table <- read.csv("./data/keywest-withfish.csv")
+  indices_table <- indices_table %>%
+    mutate(datetime = ymd_hms(datetime))
+  
+  output$corrPlot <- renderPlot({
     
+    # Select only the acoustic index columns
+    index_columns <- names(indices_table)[3:15]
+    # Calculate the correlation matrix
+    cor_matrix <- cor(indices_table[index_columns])
+    # Plot the correlation matrix
+    corrplot(cor_matrix, type = 'lower', order = 'hclust', tl.col = 'black',
+             cl.ratio = 0.2, tl.srt = 45, col = COL2('PuOr', 10))
+  })
+  
+  output$plot <- renderPlotly({
+
     fig <- plot_ly(data = indices_table)
     fig <- fig %>%
       add_trace(
@@ -45,7 +65,7 @@ server <- function(input, output) {
         xaxis = list(title = "Datetime"),
         yaxis = list(title = "ACI")
       )
-    
+
     fig
   })
 }
