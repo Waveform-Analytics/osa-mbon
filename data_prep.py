f02efb12132bd899c6aa46a6a45f1e778b6ff743
Michelle Weirathmueller <1017259+michellejw@users.noreply.github.com>
2024-05-09 11:04:10 -0400

    prep seascaper data

--- data_prep.py
+++ data_prep.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import pandas as pd
 import duckdb
+from pathlib import Path
 
 
 def normalize_df(df_in, col_names):
@@ -23,7 +24,7 @@ def normalize_df(df_in, col_names):
     df_new = df_in.copy()
     for col in col_names:
         df_zero = df_in[col] - np.nanmedian(df_in[col])
-        df_new[col] = df_zero/max(abs(df_zero))
+        df_new[col] = df_zero / max(abs(df_zero))
 
     return df_new
 
@@ -49,6 +50,21 @@ def get_fish_presence(df_in, df_fishes, unq_codes):
     return df_out
 
 
+def prep_seascaper_data(input_file):
+    """Prepare seascaper data
+
+    Args:
+        input_file: Path to seascaper file
+
+    Returns: processed data frame
+
+    """
+    df_sea = pd.read_csv(input_file)
+    df_sea["date"] = pd.to_datetime(df_sea["date"])
+
+    return df_sea
+
+
 if __name__ == "__main__":
 
     DATA_FOLDER = "shiny/shinydata/fromLiz"
@@ -69,11 +85,11 @@ if __name__ == "__main__":
         df_fish = pd.concat([df_fish, df])
 
     # Reduce columns
-    df_fish_keywest = df_fish[["start_time", "end_time", 'Low Freq (Hz)', 'High Freq (Hz)', 
-       'species', 'call variant', 'level']]
+    df_fish_keywest = df_fish[["start_time", "end_time", 'Low Freq (Hz)', 'High Freq (Hz)',
+                               'species', 'call variant', 'level']]
 
     # KEY WEST: Load the fish codes info file
-    FISH_CODES_FILE = "shiny/shinydata/fish_codes.csv"
+    FISH_CODES_FILE = "shiny/data/fish_codes.csv"
     df_fish_codes = pd.read_csv(FISH_CODES_FILE)
 
     # ################################################################################## #
@@ -83,22 +99,31 @@ if __name__ == "__main__":
     df_mayriver = pd.read_excel(MAY_RIVER_DATA_FILE, sheet_name="Data")
     df_mayriver.rename(columns={"Date": "start_time"}, inplace=True)
     df_mayriver["end_time"] = df_mayriver["start_time"] + pd.to_timedelta(2, unit="h")
-    
-    # Arrange the mayriver dataframe so it looks more like the keywest annotations
-    df_long = df_mayriver.melt(id_vars=['start_time', 'end_time'], 
+
+    # Arrange the mayriver dataframe, so it looks more like the keywest annotations
+    df_long = df_mayriver.melt(id_vars=['start_time', 'end_time'],
                                value_vars=['Silver perch',
-                                'Silver perch interruption', 'Oyster toadfish boat whistle',
-                                'Oyster toadfish grunt', 'Oyster toadfish interruption', 'Black drum',
-                                'Black drum interruption', 'Spotted seatrout',
-                                'Spotted seatrout interruption', 'Red drum', 'Red drum interruption',
-                                'Atlantic croaker', 'Weakfish', 'Fish interruption cause',
-                                'Bottlenose dolphin echolocation', 'Bottlenose dolphin burst pulses',
-                                'Bottlenose dolphin whistles', 'Vessel'],
+                                           'Silver perch interruption', 'Oyster toadfish boat whistle',
+                                           'Oyster toadfish grunt', 'Oyster toadfish interruption', 'Black drum',
+                                           'Black drum interruption', 'Spotted seatrout',
+                                           'Spotted seatrout interruption', 'Red drum', 'Red drum interruption',
+                                           'Atlantic croaker', 'Weakfish', 'Fish interruption cause',
+                                           'Bottlenose dolphin echolocation', 'Bottlenose dolphin burst pulses',
+                                           'Bottlenose dolphin whistles', 'Vessel'],
                                var_name='species', value_name='is_present')
-    df_final = df_long[df_long['is_present'] != 0]
-    mr_codes = df_fish_codes[df_fish_codes["may_river"]==1]
+    df_final = df_long[df_long['is_present'] != 0].copy()
+    mr_codes = df_fish_codes[df_fish_codes["Dataset"] == "May River"]
     df_final["species"] = df_final["species"].map(dict(zip(mr_codes["species"], mr_codes["code"]))).copy()
     df_fish_mayriver = df_final.copy()
+
+    # ################################################################################## #
+    # GRAY'S REEF ANNOTATIONS (VESSELS)
+    GRAYS_REEF_DATA_FILE = ("shiny/shinydata/fromLiz/GraysReef_GR01/sanctsound_products_detections_"
+                            "gr01_sanctsound_gr01_01_ships_data_SanctSound_GR01_01_ships.csv")
+    df_ships_grays = pd.read_csv(GRAYS_REEF_DATA_FILE)
+    df_ships_grays.columns = ["start_time", "end_time", "type"]
+    df_ships_grays["start_time"] = pd.to_datetime(df_ships_grays["start_time"])
+    df_ships_grays["end_time"] = pd.to_datetime(df_ships_grays["end_time"])
 
     # ################################################################################## #
     # ACOUSTIC INDICES FILES - COMPILATION #
@@ -112,13 +137,13 @@ if __name__ == "__main__":
         df = pd.read_csv(file)
         df["file_id"] = idx
         df_aco = pd.concat([df_aco, df])
-    # The date strings in "Date" column are not consistently formatted so we use "mixed"
+    # The date strings in "Date" column are not consistently formatted, so we use "mixed"
     df_aco['start_time'] = pd.to_datetime(df_aco['Date'], format="mixed")
     # Drop duplicate rows 
-    df_aco = df_aco.drop_duplicates(subset=['Date', 'Dataset', 'Sampling_Rate_kHz', 'FFT', 'Duration_sec', \
-                                          'Thresholds_Hz', 'Filename', "file_id"], keep="first")
+    df_aco = df_aco.drop_duplicates(subset=['Date', 'Dataset', 'Sampling_Rate_kHz', 'FFT', 'Duration_sec',
+                                            'Thresholds_Hz', 'Filename', "file_id"], keep="first")
 
-    ## Generate an "end_time" column in df_aco_norm
+    # # Generate an "end_time" column in df_aco_norm
     # Sort by start_time and file_id
     df_aco = df_aco.sort_values(by=['file_id', 'start_time'])
     # Calculate differences within each file/dataset and convert to seconds
@@ -131,17 +156,29 @@ if __name__ == "__main__":
     # Get the end time by adding median diff to start_time
     df_aco['end_time'] = df_aco['start_time'] + pd.to_timedelta(df_aco['median_diff'], unit='s')
     # Tidy up the dataframe
-    df_aco = df_aco.drop(columns=['file_id','time_diff', 'median_diff'])
+    df_aco = df_aco.drop(columns=['file_id', 'time_diff', 'median_diff'])
 
-    # Fix: Caser Creek to Caesar Creek
+    # Fix: Correct typo
     df_aco['Dataset'] = df_aco['Dataset'].replace('Caser Creek', 'Caesar Creek')
 
     # Normalize the indices
     df_aco_norm = normalize_df(df_aco, df_aco.columns[7:-2])
 
+    # ################################################################################## #
+    # SATELLITE WATER CLASS DATA
+    SEASCAPER_FOLDER = Path("shiny/shinydata/fromLiz/All_SeascapeR")
+    seascaper_files = list(SEASCAPER_FOLDER.glob("*.csv"))
+
+    s_list = []
+    for s_file in seascaper_files:
+        df_temp = pd.read_csv(s_file)
+        df_temp["date"] = pd.to_datetime(df_temp["date"])
+        s_list.append(df_temp)
+
+    df_seascaper = pd.concat(s_list)
 
     # ################################################################################## #
-    ## SAVE DATAFRAMES TO PARQUET TABLES
+    # # SAVE DATAFRAMES TO PARQUET TABLES
     df_aco_valid = df_aco.dropna()
     df_aco_valid.to_parquet(OUT_FOLDER + '/t_aco.parquet')
 
@@ -158,15 +195,29 @@ if __name__ == "__main__":
     df_fish_mayriver_valid = df_fish_mayriver.dropna().copy()
     df_fish_mayriver_valid.to_parquet(OUT_FOLDER + '/t_fish_mayriver.parquet')
 
+    # Grays Reef ship data
+    df_ships_grays_valid = df_ships_grays.dropna().copy()
+    df_ships_grays_valid.to_parquet(OUT_FOLDER + "/t_ships_grays.parquet")
+
+    # Seascaper
+    df_seascaper.to_parquet(OUT_FOLDER + "/t_seascaper")
+
     # ################################################################################## #
-    ## SAVE PARQUET TABLES TO DUCKDB DATABASE FILE`
-    DB_FILE = 'shiny/mbon.duckdb'
+    # SAVE PARQUET TABLES TO DUCKDB DATABASE FILE`
+    DB_FILE = 'shiny/data/mbon.duckdb'
     if os.path.exists(DB_FILE):
         os.remove(DB_FILE)
 
     con = duckdb.connect(DB_FILE)
     con.execute("CREATE TABLE t_aco AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/t_aco.parquet')")
-    con.execute("CREATE TABLE t_aco_norm AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/t_aco_norm.parquet')")
-    con.execute("CREATE TABLE t_fish_keywest AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/t_fish_keywest.parquet')")
-    con.execute("CREATE TABLE t_fish_mayriver AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/t_fish_mayriver.parquet')")
+    con.execute("CREATE TABLE t_aco_norm AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/"
+                "t_aco_norm.parquet')")
+    con.execute("CREATE TABLE t_fish_keywest AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/"
+                "t_fish_keywest.parquet')")
+    con.execute("CREATE TABLE t_fish_mayriver AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/"
+                "t_fish_mayriver.parquet')")
+    con.execute("CREATE TABLE t_ships_grays AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/"
+                "t_ships_grays.parquet')")
+    con.execute("CREATE TABLE t_seascaper AS SELECT * FROM read_parquet('shiny/shinydata/prepped_tables/"
+                "t_seascaper.parquet')")
     con.close()
---
+++ shiny/data/prep_data.R
@@ -0,0 +1,65 @@
+# Prep data and variables
+test_string <- "testing testing"
+
+# Establish connection to DuckDB
+con <- dbConnect(duckdb::duckdb(), "data/mbon.duckdb")
+
+# Key West Annotations
+df_fish_keywest <- dbReadTable(con, "t_fish_keywest") %>%
+  select(start_time, end_time, species)
+
+# May River Annotations
+df_fish_mayriver <- dbReadTable(con, "t_fish_mayriver") %>%
+  select(start_time, end_time, species)
+
+# Combine the fish annotation data
+df_fish_keywest$Dataset <- "Key West"
+df_fish_mayriver$Dataset <- "May River"
+df_fish <- rbind(df_fish_keywest, df_fish_mayriver)
+
+# Acoustic indices.
+df_aco <- dbReadTable(con, "t_aco")
+df_aco_norm <- dbReadTable(con, "t_aco_norm")
+
+dbDisconnect(con)
+
+# Annotations lookup table
+fish_codes <- read_csv("data/fish_codes.csv", show_col_types = FALSE)
+
+# Get all of the unique datasets
+unique_datasets <- df_aco %>%
+  distinct(Dataset) %>%
+  pull(Dataset)
+
+# Get all of the unique sample rates for the selected dataset
+unique_sr <- df_aco %>%
+  filter(Dataset == unique_datasets[1]) %>%
+  select(Sampling_Rate_kHz) %>%
+  distinct() %>%
+  pull(Sampling_Rate_kHz)
+
+# Get unique durations based on previous selections
+unique_durations <- df_aco %>%
+  filter(Dataset == unique_datasets[1]) %>%
+  filter(Sampling_Rate_kHz == unique_sr[1]) %>%
+  select(Duration_sec) %>%
+  distinct() %>%
+  pull(Duration_sec)
+
+# Extract the column names that represent all acoustic indices
+col_names = names(df_aco)
+index_columns_all <- col_names[8:(length(col_names)-4)]
+
+# A subset of the index columns - update to pre-select a subset
+# index_columns <- index_columns_all[1:10]
+index_columns <- index_columns_all
+
+# Unique datasets with annotations
+unique_datasets_ann <- c("Key West", "May River")
+unique_species <- fish_codes %>%
+  filter(Dataset == unique_datasets_ann[1]) %>%
+  distinct(code) %>% pull(code)
+
+
+
+
